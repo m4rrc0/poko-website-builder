dc2a6e1545e42e3874882ebbfc6e652f5f0a2a2c
m4rrc0 <github-h9s2@m4rr.co>
2026-01-26 16:16:25 +0100

    feat: improve image shortcode to capture class and id + any other attributes

--- src/config-11ty/shortcodes/components/image.js
+++ src/config-11ty/shortcodes/components/image.js
@@ -1,5 +1,6 @@
 import Image from "@11ty/eleventy-img";
 import deepmerge from "deepmerge";
+import { removeUndefinedProps } from "../../../utils/objects.js";
 import { imageTransformOptions } from "../../plugins/imageTransform.js";
 import { WORKING_DIR } from "../../../../env.config.js";
 
@@ -16,10 +17,45 @@ export async function image(args) {
     sizes,
     wrapper,
     class: className,
+    id,
     style,
     imgAttributes,
+    // Other possible image arguments that could be passed through the `imgAttrs` field in the CMS or manually
+    name,
+    height,
+    srcset,
+    crossorigin,
+    usemap,
+    ismap,
+    referrerpolicy,
+    draggable,
+    hidden,
+    tabindex,
+    contenteditable,
+    dir,
+    lang,
+    spellcheck,
+    // The rest are possibly options to pass as shortcode options
     ...opts
   } = args;
+
+  const otherArgs = removeUndefinedProps({
+    name,
+    height,
+    srcset,
+    crossorigin,
+    usemap,
+    ismap,
+    referrerpolicy,
+    draggable,
+    hidden,
+    tabindex,
+    contenteditable,
+    dir,
+    lang,
+    spellcheck,
+  });
+
   let wrapperTag = wrapper ? wrapper.split(" ")[0] : "";
   // wrapperTag = wrapperTag || (width ? "p" : "");
   // TODO: compute sizes from widths
@@ -49,9 +85,11 @@ export async function image(args) {
               ((className || imgAttributes?.class) && {
                 class: className || imgAttributes?.class || "",
               })),
+            ...(id && { id }),
             // ...((width && { style: `max-width:${width}px;${style || ""}` }) ||
             //   (style && { style })),
             ...(style && { style }),
+            ...otherArgs,
           },
         },
       },
