8d887e5bc43a1b0b1d0fe103b4eb9ac1796f1cde
m4rrc0 <github-h9s2@m4rr.co>
2026-09-11 22:27:54 +0200

    fix: default collection partial

--- src/content/_partials/_collectionItem.11ty.js
+++ src/content/_partials/_collectionItem.11ty.js
@@ -1,6 +1,6 @@
 export default async function (data) {
   const { title, description, image } = data.pagePreview;
-  const imageStr = image ? `<img src="${image.src}" alt="">` : "";
+  const imageStr = image?.src ? `<img src="${image.src}" alt="">` : "";
   const titleStr = title ? `<h3><a href="${data.url}">${title}</a></h3>` : "";
   const descriptionStr = description ? `<p>${description}</p>` : "";
   const output = [imageStr, titleStr, descriptionStr]
